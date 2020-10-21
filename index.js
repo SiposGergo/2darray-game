208fcb341f418ef7750a0996612c2a53fc805c7a
Sipos Gergő <prosipinho@gmail.com>
2020-10-21 15:02:54 +0200

    fillMap() v1

---
+++ index.js
@@ -0,0 +1,12 @@
+// const fillMap = require('./map').fillMap;
+// const valamimás = require('./map').valamimás;
+const { fillMap } = require('./map');
+
+const width = 30;
+const height = 20;
+
+let map = [];
+const apples = [];
+const player = { pos: { x: 1, y: 1 }, score: 0 };
+
+map = fillMap(width, height, apples, player);
