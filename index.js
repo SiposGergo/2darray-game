0f59d8fd4c7765a21b50c42f2281ca5fcf6dee4e
Sipos Gergő <prosipinho@gmail.com>
2020-10-21 15:48:39 +0200

    Draw table

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // const fillMap = require('./map').fillMap;
 // const valamimás = require('./map').valamimás;
-const { fillMap } = require('./map');
+const { fillMap, drawMap } = require('./map');
 
-const width = 30;
-const height = 20;
+const width = 20;
+const height = 10;
 
 let map = [];
 const apples = [];
 const player = { pos: { x: 1, y: 1 }, score: 0 };
 
 map = fillMap(width, height, apples, player);
+drawMap(map);
--- map.js
+++ map.js
@@ -1,11 +1,20 @@
+// import tableImport from 'table';
+const { table } = require('table');
+
 const fillMap = (width, height, apples, player) => {
-  const array = new Array(width);
-  for (let i = 0; i < width; i++) {
-    array[i] = new Array(height);
+  const array = new Array(height);
+  for (let i = 0; i < height; i++) {
+    array[i] = new Array(width);
   }
   return array;
 };
 
+const drawMap = (map) => {
+  const text = table(map);
+  console.log(text);
+};
+
 module.exports = {
-  fillMap
+  fillMap,
+  drawMap
 };
